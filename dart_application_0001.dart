550635415f7af851e55f0cffcec5f3f99e7e7ae7
VladVolodin-123 <118240818+VladVolodin-123@users.noreply.github.com>
2022-11-26 08:39:45 +0700

    Update dart_application_0001.dart

--- dart_application_0001.dart
+++ dart_application_0001.dart
@@ -748,6 +748,10 @@
 
 
 // 6.1
+
+
+
+
 // import 'dart:convert';
 // import 'dart:io';
 // import 'dart:async';
@@ -772,6 +776,61 @@
 // }
 // print(done);
 // }
+
+
+
+
+
+
+
+
+//6.2 
+
+
+
+// import 'dart:convert';
+// import 'dart:io';
+// import 'dart:async';
+// import 'dart:math';
+
+// void main() async {
+// final file = File('numsTask1.txt');
+// String nums_txt = "";
+// Stream<String> lines = file
+// .openRead()
+// .transform(utf8.decoder) // Decode bytes to UTF-8.
+// .transform(LineSplitter()); // Convert stream to individual lines.
+// try {
+// await for (var line in lines) {
+// nums_txt += line;
+// nums_txt +=" ";
+// }
+// } catch (e) {
+// print('Error: $e');
+// }
+// print(nums_txt);
+
+// }
+
+
+
+
+
+
+//6.3 
+
+
+// import 'dart:convert';
+// import 'dart:io';
+// import 'dart:async';
+// import 'dart:math';
+// void main(){
+// int nums = int.parse(stdin.readLineSync()!);
+// if(nums%10==0 && nums%2==0){
+// print("является");
+// } else{print("не является");}
+// }
+
 
 
 
